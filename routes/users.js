d6764c94ec3b54e1050dedb89875835cdd52171b
freaklucas <lucas.oliveira.freak@gmail.com>
2021-10-28 23:21:31 -0300

    projeto ok, deus abençoe

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,3 @@
-
-// const index = require('../views/index.html');
-
 const express = require("express");
 const router = express.Router();
 
@@ -10,16 +7,5 @@ const AuthController = require("../src/users/user.controller");
 router.post("/signup", cleanBody, AuthController.Signup);
 
 router.post("/login", cleanBody, AuthController.Login);
-
-router.post("/cadastrar", cleanBody, AuthController.Login);
-
-// router.get("/add", (req, res) => {
-//     res.render("add_content", { title: "Add content"});
-// });
-
-
-
-
-
 
 module.exports = router;
